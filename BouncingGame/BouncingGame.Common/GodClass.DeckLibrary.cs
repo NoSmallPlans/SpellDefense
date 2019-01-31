db1403aca068bd50cfd7eba896998f52ca6e48b9
Geoboardman <saville_rich@gmail.com>
2019-01-30 21:38:29 -0800

    Fixed card shuffling and drawing, added deck library

---
+++ BouncingGame/BouncingGame.Common/GodClass.DeckLibrary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellDefense.Common
+{
+    public static partial class GodClass
+    {
+        public static Dictionary<string, string> Decks = new Dictionary<string, string>()
+        {
+             {
+                "deck1",
+                @"{
+                    'cards': [
+                        {
+                            'name': 'fireball',
+                            'amount': '2'
+                        },
+                        {
+                            'name': 'Haste',
+                            'amount': '2'
+                        },
+                    ]
+                }"
+            },
+             {
+                "deck2",
+                @"{
+                    'cards': [
+                        {
+                            'name': 'Add Ranged',
+                            'amount': '2'
+                        },
+                        {
+                            'name': 'Slow',
+                            'amount': '2'
+                        },
+                    ]
+                }"
+            }
+        };
+    }
+}
